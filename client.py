4b03acd0ed8767c4edf65a257cea0b05013862e4
Zihang XIE <83111213+mopian777@users.noreply.github.com>
2025-12-01 22:24:35 +0100

    Update client.py

--- client.py
+++ client.py
@@ -5,21 +5,20 @@ import sys
 import random
 
 if len(sys.argv) < 3:
-    print("Usage: python3 {} <client_id> <server_ip>".format(sys.argv[0]))
+    print("Usage: python3 {} <client_id> <server_ip_or_host>".format(sys.argv[0]))
     sys.exit(1)
 
 client_id = sys.argv[1]
-SERVER_IP = sys.argv[2]
+server_host = sys.argv[2]   # 可以是 '10.x.x.x' 也可以是 'server'
 PORT = 5000
 
-# 模拟本地“训练结果”
 local_value = random.uniform(0.0, 10.0)
 data = {"client_id": client_id, "value": local_value}
 print("[{}] Local value = {}".format(client_id, local_value))
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
-    s.connect((SERVER_IP, PORT))
+    s.connect((server_host, PORT))   # 由系统自己解析 server_host
     s.sendall(json.dumps(data).encode("utf-8"))
     resp_raw = s.recv(4096).decode("utf-8")
     resp = json.loads(resp_raw)
